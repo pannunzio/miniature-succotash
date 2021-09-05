0cd28108449dd7a18e1cfd1198d2f19f8aa6e121
pannunzio <mariana_queiroz@yahoo.com>
2021-09-04 23:03:06 -0400

    switch from react to next

---
+++ nextjs-blog/pages/_app.js
@@ -0,0 +1,5 @@
+import '../styles/global.css'
+/* eslint-disable react/prop-types */
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <h1 className="title">
+          Read{' '}
+          <Link href="/posts/first-post">
+            this page!
+          </Link>
+        </h1>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
+}
